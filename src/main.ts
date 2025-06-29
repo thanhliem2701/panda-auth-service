fe9e15e84b57c03974ab5f322f13ed3e8bbc0f8b
thanhliem2701 <thanhliem2701@gmail.com>
2025-06-29 21:56:38 +0800

    add url to .env

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  const appContext = await NestFactory.createApplicationContext(AppModule);
+  const configService = appContext.get(ConfigService);
+  const rabbitUrl = configService.get<string>('AMQP_URL');
+  const queueName = configService.get<string>('AMQP_QUEUE');
+  if (!rabbitUrl) {
+    throw new Error('RABBITMQ_URL is not defined in the configuration');
+  }
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule,{
     transport: Transport.RMQ,
     options:{
-      urls:['amqp://admin:1234@localhost:5672'],
-      queue: 'auth_queue',
+      urls:[rabbitUrl],
+      queue: queueName,
       queueOptions:{
         durable:true,
       }
